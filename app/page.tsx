d3ef9568647329ceaad86b9099f21993e922dfd3
ilhamhlmi <ilhamhilmi206@gmail.com>
2026-03-16 00:02:39 +0700

    x hamburger line, dropdown navbar mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,44 @@
+"use client";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { useState, useEffect } from "react";
 
 export default function Home() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
       {/* Mobile Start */}
       <header className="xl:hidden w-full z-50 fixed top-0 left-0 flex items-center justify-center px-6">
-        <nav className="bg-white xl:w-3/4 w-full rounded-full px-6 py-3 mt-5 flex items-center justify-between">
-          <button className="cursor-pointer bg-white">
-            <span className="hamburger-line"></span>
-            <span className="hamburger-line"></span>
+        <nav className="bg-white xl:w-3/4 w-full rounded-full px-6 py-3 mt-5 flex items-center justify-between relative">
+          <button onClick={() => setIsOpen(!isOpen)} className="cursor-pointer bg-white">
+            <span className={`w-6.5 h-0.5 my-2 block bg-primary transform transition-all duration-300 ${isOpen ? 'rotate-46 translate-y-1.25' : 'rotate-0 translate-y-0 scale-100'}`}></span>
+            <span className={`w-6.5 h-0.5 my-2 block bg-primary transform transition-all duration-300 ${isOpen ? '-rotate-46 -translate-y-1.25' : 'rotate-0 translate-y-0'}`}></span>
           </button>
-          <a className="border border-midnight rounded-full px-3 py-2  text-pearl bg-midnight font-montserrat">Download CV</a>
+          <a className="border border-primary rounded-full px-3 py-2  text-white bg-primary font-montserrat">Download CV</a>
+
+          <div className={`bg-white absolute left-0 top-full mt-3.5 rounded-4xl w-full px-6 transition-all duration-250 ease-in text-center flex flex-col space-y-5 ${isOpen ? 'max-h-100 opacity-100 py-4' : 'max-h-0 opacity-0 py-0'}`}>
+            <a href="" className="text-primary font-montserrat font-semibold text-xl">Home</a>
+            <a href="" className="text-primary font-montserrat font-semibold text-xl">About</a>
+            <a href="" className="text-primary font-montserrat font-semibold text-xl">Contact</a>
+          </div>
+
         </nav>
       </header>
       {/* Mobile End */}
 
       {/* Desktop Start */}
       <header className="hidden w-full z-50 fixed top-0 left-0 xl:flex items-center justify-center px-6">
-        <nav className="bg-white xl:w-3/4 w-full rounded-full px-6 py-4 mt-5 flex items-center justify-between">
+        <nav className="bg-white xl:w-3/4 w-full rounded-full px-6 py-3 mt-5 flex items-center justify-between">
           <div>
-            <a href="" className="text-midnight font-montserrat font-semibold">Ilham Hilmi</a>
+            <a href="" className="text-primary font-montserrat font-semibold">Ilham Hilmi</a>
           </div>
-          <div className="space-x-15">
-            <a href="" className="text-midnight font-montserrat ">Home</a>
-            <a href="" className="text-midnight font-montserrat ">About</a>
-            <a href="" className="text-midnight font-montserrat ">Contact</a>
-            <a href="" className="border border-midnight rounded-full px-3 py-2  text-pearl bg-midnight font-montserrat">Download CV</a>
+          <div className="space-x-12">
+            <a href="" className="text-primary font-montserrat text-sm font-semibold hover:text-teal-600 duration-200">Home</a>
+            <a href="" className="text-primary font-montserrat text-sm font-semibold hover:text-teal-600 duration-200">About</a>
+            <a href="" className="text-primary font-montserrat text-sm font-semibold hover:text-teal-600 duration-200">Contact</a>
+            <a href="" className="border border-primary rounded-full px-3 py-1.5  text-white bg-primary font-montserrat text-sm hover:bg-teal-600 hover:border-teal-600 hover:shadow-md duration-200">Download CV</a>
           </div>
         </nav>
       </header>
